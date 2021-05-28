1fd333778b866b9b065091c2a44c568efac14d2d
XuyingSwift <xuyingswift@gmail.com>
2021-05-28 10:19:31 -0600

    cartIcon

--- src/components/Header/Header.component.jsx
+++ src/components/Header/Header.component.jsx
@@ -4,11 +4,13 @@ import {connect} from 'react-redux';
 
 import {Link} from 'react-router-dom';
 
-import {ReactComponent as Logo} from '../../asserts/fish.svg';
+import {ReactComponent as Logo} from '../../assets/fish.svg';
 
 import './Header.styles.scss';
 
 import {auth} from '../../firebase/firebase.utils'
+
+import CartIcon from '../Cart-Icon/CartIcon.component';
 
 const Header = ({currentUser}) => {
     return (
@@ -19,10 +21,13 @@ const Header = ({currentUser}) => {
             <div className='options'>
                 <Link className='option' to='/shop'>Shop</Link>
                 <Link className='option' to='/shop'>Contact</Link>
+                
                 { currentUser ? 
                     <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div> 
                     : <Link className='option' to='/signin'>Sign in</Link>}
+                <CartIcon/>
             </div>
+            
         </div>
     )
 }
